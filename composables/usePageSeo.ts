f42d33ed418ffc724144027197b156757d95e29a
zi <z4none@gmail.com>
2025-06-17 16:52:02 +0800

    新增语言文件

--- composables/usePageSeo.ts
+++ composables/usePageSeo.ts
@@ -5,12 +5,18 @@ const SUPPORTED_LOCALES = [
   { code: 'en', lang: 'en' },
   { code: 'ar', lang: 'ar' },
   { code: 'es', lang: 'es' },
+  { code: 'es-419', lang: 'es-419' },
   { code: 'hi', lang: 'hi' },
   { code: 'id', lang: 'id' },
+  { code: 'it', lang: 'it' },
   { code: 'ja', lang: 'ja' },
   { code: 'ko', lang: 'ko' },
   { code: 'pt', lang: 'pt' },
+  { code: 'pt-br', lang: 'pt-BR' },
   { code: 'th', lang: 'th' },
+  { code: 'tr', lang: 'tr' },
+  { code: 'fr', lang: 'fr' },
+  { code: 'de', lang: 'de' },
   { code: 'vi', lang: 'vi' },
   { code: 'zh-TW', lang: 'zh-Hant' }
 ]
@@ -32,8 +38,6 @@ export const usePageSeo = () => {
       ogTitle: () => t('seo.ogTitle'),
       ogDescription: () => t('seo.ogDescription'),
       ogSiteName: () => t('siteName'),
-      twitterTitle: () => t('seo.ogTitle'),
-      twitterDescription: () => t('seo.ogDescription'),
       canonical: canonicalUrl,
       alternateLanguages: () => {
         const links = {}
