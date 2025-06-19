de12ab7a772b0465ab46494f69405a70ba19c750
zi <z4none@gmail.com>
2025-06-19 10:44:48 +0800

    nuxt 重写时忘记了 GA

--- README.md
+++ README.md
@@ -73,3 +73,12 @@ bun run preview
 ```
 
 Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
+
+# SEO
+
+google GA
+
+bing
+
+yandex
+
--- app.vue
+++ app.vue
@@ -14,6 +14,20 @@ useHead({
   link: [
     { rel: 'icon', type: 'image/png', href: '/favicon.png' },
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ],
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-90XNEYE25X',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-90XNEYE25X');
+      `
+    }
   ]
 });
 </script>
