d3272b70c9a66b7d45d1b5516ff7cfaeac2574b3
zi <z4none@gmail.com>
2025-08-15 15:59:55 +0800

    i18n utils.js 动态读取 sites 配置

--- i18n/utils.js
+++ i18n/utils.js
@@ -1,17 +1,15 @@
 
+import sites from "~/sites"
+
 // 根据子站路径返回对应的 i18n 字符串
 // 如果子站路径不存在，则返回默认的 i18n 字符串
 export const _t = (str) => {
     const route = useRoute()
     const { t, te } = useI18n()
-    if(route.path.endsWith('-mp3') && te(`mp3_${str}`)) {
-        return t(`mp3_${str}`)
-    }
-    else if(route.path.endsWith('-downloader') && te(`downloader_${str}`)) {
-        return t(`downloader_${str}`)
-    }
-    else if(route.path.endsWith('-transcript-generator') && te(`transcript_${str}`)) {
-        return t(`transcript_${str}`)
+    for (const site of sites) {
+        if (route.path.endsWith(site.url) && te(`${site.i18n}_${str}`)) {
+            return t(`${site.i18n}_${str}`)
+        }
     }
     else if(route.path.endsWith('-m4a') && te(`m4a_${str}`)) {
         return t(`m4a_${str}`)
@@ -23,14 +21,10 @@ export const _t = (str) => {
 export const _te = (str) => {
     const route = useRoute()
     const { te } = useI18n()
-    if(route.path.endsWith('-mp3')) {
-        return te(`mp3_${str}`)
-    }
-    else if(route.path.endsWith('-downloader')) {
-        return te(`downloader_${str}`)
-    }
-    else if(route.path.endsWith('-transcript-generator')) {
-        return te(`transcript_${str}`)
+    for (const site of sites) {
+        if (route.path.endsWith(site.url) && te(`${site.i18n}_${str}`)) {
+            return te(`${site.i18n}_${str}`)
+        }
     }
     else if(route.path.endsWith('-m4a')) {
         return te(`m4a_${str}`)
@@ -41,14 +35,10 @@ export const _te = (str) => {
 export const _tm = (str) => {
     const route = useRoute()
     const { tm, te } = useI18n()
-    if(route.path.endsWith('-mp3') && te(`mp3_${str}`)) {
-        return tm(`mp3_${str}`)
-    }
-    else if(route.path.endsWith('-downloader') && te(`downloader_${str}`)) {
-        return tm(`downloader_${str}`)
-    }
-    else if(route.path.endsWith('-transcript-generator') && te(`transcript_${str}`)) {
-        return tm(`transcript_${str}`)
+    for (const site of sites) {
+        if (route.path.endsWith(site.url) && te(`${site.i18n}_${str}`)) {
+            return tm(`${site.i18n}_${str}`)
+        }
     }
     else if(route.path.endsWith('-m4a') && te(`m4a_${str}`)) {
         return tm(`m4a_${str}`)
